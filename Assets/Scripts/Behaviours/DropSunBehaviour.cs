845ec51778cc431971c84d7efc99964fc2d04e3e
Petra Béczi <beczi@student.chalmers.se>
2020-12-14 14:17:40 +0100

    Implements throwing of the sun with force
    Additionally, changed the project setting to disable collision detection to a new layer called: "NoCollWithSun" and added all gameobjects with this layer.

---
+++ Assets/Scripts/Behaviours/DropSunBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropSunBehaviour : MonoBehaviour
+{
+    public float throwForce = 1.5f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.right * throwForce, ForceMode.Impulse);
+    }
+
+}
