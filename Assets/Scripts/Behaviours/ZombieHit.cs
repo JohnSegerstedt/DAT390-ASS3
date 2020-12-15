0db3e885e63bf3f8e50dc9f79692552f92907098
Petra Béczi <beczi@student.chalmers.se>
2020-12-15 16:06:57 +0100

    Implements pea hit and zombie death particles

---
+++ Assets/Scripts/Behaviours/ZombieHit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHit : MonoBehaviour
+{
+    public ParticleSystem peaParticles;
+    public ParticleSystem dirtParticles;
+    public HealthBehaviour healthBehaviour;
+    private float health;
+    public GameObject hat;
+
+    void Start()
+    {
+        peaParticles.Stop();
+        dirtParticles.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = healthBehaviour.GetCurrentHealth();
+
+        if (health < 50.0f)  // Remove hat
+        {
+            hat.SetActive(false);
+        }
+
+        if (health <20)
+        {
+            dirtParticles.Emit(10);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Pea")
+        {
+            peaParticles.Emit(10);
+        }
+    }
+}
